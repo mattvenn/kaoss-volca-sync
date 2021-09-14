2b5c148c2d3d66c157546d9c2a4bd434bedff87d
matt venn <matt@mattvenn.net>
2021-09-14 20:51:08 +0200

    add the button because I forgot to push or commit. fix button behaviour, change to 16 leds

--- src/clock.ino
+++ src/clock.ino
@@ -1,8 +1,10 @@
 #include <MIDI.h>
 #define LED 13
 #include "FastLED.h"
-#define NUM_LEDS 32 
+#define NUM_LEDS 16 
 #define DATA_PIN 17 
+#define SWITCH_P 8
+#define SWITCH_N 6
 
 unsigned int clock_count = 0;
 unsigned long tick_time = 0;
@@ -10,13 +12,14 @@ unsigned long last_count = 0;
 unsigned int sync_count = 0;
 unsigned int led_count = 0;
 bool demo_mode = 0;
+bool button_pressed = 0;
 CRGB bar_complete = CRGB(0, 30, 0);
 CRGB bar_marker   = CRGB(2, 10, 0);
 
 // midi clock is 24 pulses per quarter note https://en.wikipedia.org/wiki/MIDI_beat_clock
 #define CLOCKS_PER_SYNC 12
 #define SYNC_LEN 5 // ms
-#define SYNCS_PER_LED 12 // so should be 1 led per bar
+#define SYNCS_PER_LED 8 // so should be 1 led per bar
 
 #define STATE_WAIT 0
 #define STATE_SYNC_PULSE 1
@@ -30,6 +33,10 @@ CRGB leds[NUM_LEDS];
 void setup()
 {
     pinMode(LED, OUTPUT);
+    pinMode(SWITCH_N, OUTPUT);
+    pinMode(SWITCH_P, INPUT_PULLUP);
+    digitalWrite(SWITCH_N, LOW);
+
     digitalWrite(LED, 1);
     Serial2.begin(31250);
     FastLED.addLeds<WS2812, DATA_PIN>(leds, NUM_LEDS);
@@ -60,6 +67,21 @@ void loop()
             last_count = millis();
             clock_count += 1;
         }
+
+    if(digitalRead(SWITCH_P) == false) // pressed
+    {
+        if(button_pressed == false)
+        {
+        button_pressed = true;
+        sync_count = 0;
+        reset_leds();
+        leds[NUM_LEDS-1] = bar_complete;
+        led_count = 1;
+        FastLED.show();
+        }
+    }
+    else
+        button_pressed = false;
 
     switch(state) 
     {
